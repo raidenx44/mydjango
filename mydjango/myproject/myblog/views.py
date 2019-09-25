ef8331d23557d2dd5485b49c001b77422fd36247
raidenx44 <raidenx44@gmail.com>
2019-09-26 03:05:13 +0800

    Add files via upload
    Model + View
    
    Following the tuts below
    https://www.youtube.com/watch?v=6y16CEy12KU&list=PLlWVb5gnhtmYfmwuYsNpl2V8R37Bb2BPF&index=2

--- mydjango/myproject/myblog/views.py
+++ mydjango/myproject/myblog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
-def index(request):
+def BlogView(request):
+	tit = myblog.object.all()
+	return render(request, '', 'Title: ', tit)
+
+"""def index(request):
 	return HttpResponse("Hello django!")
+"""
